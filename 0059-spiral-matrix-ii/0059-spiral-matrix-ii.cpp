a09643c618c2f9ffcc27cf9c5148816c02ab7e03
Suy65 <115409094+Suy65@users.noreply.github.com>
2023-05-10 18:09:33 +0530

    Time: 5 ms (6.36%), Space: 6.5 MB (90.43%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<int>spiral;
+        vector<vector<int>>matrix(n,vector<int>(n,0));
+        int count =0;
+        int total = n*n;
+        int top =0;
+        int bottom =n-1;
+        int left=0;
+        int right =n-1;
+        
+        for( int i= 0; i<n*n ;i++){
+            spiral.push_back(i+1);
+        }
+        while(count<total){
+           for( int i = left ; count<total && i<=right;i++){
+               if(matrix[top][i] == 0){  
+                  matrix[top][i] = spiral[count++];
+               }
+            }
+          top++;
+          for( int i =top ; count<total && i<=bottom;i++){
+                if(matrix[i][right] == 0){
+                    matrix[i][right] = spiral[count];
+                }
+                count++;
+            }
+          right--;
+          for( int i = right ;count<total && i>=left ;i--){
+               if(matrix[bottom][i] == 0){
+                   matrix[bottom][i] = spiral[count];
+               }
+               count++;
+           } 
+          bottom--;
+          for( int i = bottom; count<total && i>=top ;i--){
+               if(matrix[i][left] == 0){
+                   matrix[i][left] = spiral[count];
+               }
+               count++;
+           }
+          left++;   
+         }
+        return matrix;
+    }
+};
