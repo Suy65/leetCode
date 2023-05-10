69663cfe46d1dc82c798efd42a5bc9ba5ec75d06
Suy65 <115409094+Suy65@users.noreply.github.com>
2023-05-10 18:23:55 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (90.43%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-       // vector<int>spiral;
-        vector<vector<int>>matrix(n,vector<int>(n,0));
-        int count =1;
-        int total = n*n;
-        int top =0;
-        int bottom =n-1;
-        int left=0;
-        int right =n-1;
+        vector<vector<int>> ans(n, vector<int>(n,0));
         
-       // for( int i= 0; i<n*n ;i++){
-       //     spiral.push_back(i+1);
-       // }
-        while(count<=total){
-           for( int i = left ; count<=total && i<=right;i++){
-               if(matrix[top][i] == 0){  
-                  matrix[top][i] = count++;
-               }
-            }
-          top++;
-          for( int i =top ; count<total && i<=bottom;i++){
-                if(matrix[i][right] == 0){
-                    matrix[i][right] = count++;
-                }
-                
-            }
-          right--;
-          for( int i = right ;count<=total && i>=left ;i--){
-               if(matrix[bottom][i] == 0){
-                   matrix[bottom][i] = count++;
-               }
-               
-           } 
-          bottom--;
-          for( int i = bottom; count<=total && i>=top ;i--){
-               if(matrix[i][left] == 0){
-                   matrix[i][left] = count++;
-               }
-               
-           }
-          left++;   
-         }
-        return matrix;
+        int left=0;
+        int right=n-1;
+        int top=0;
+        int bottom=n-1;
+        int count=1;
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left; i<=right; i++)
+                ans[top][i]=count++;
+            top++;
+
+            for(int i=top; i<=bottom; i++)
+                ans[i][right]=count++;
+            right--;
+
+           // if(top<=bottom)
+            //{
+                for(int i=right; i>=left; i--)
+                    ans[bottom][i]=count++;
+                bottom--;
+            //}
+            //if(left<=right)
+            //{
+                for(int i=bottom; i>=top; i--)
+                    ans[i][left]=count++;
+                left++;
+            //}
+
+        }
+        return ans;
+        
     }
 };
