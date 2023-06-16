d57be3ee8367ca4d96efd5cf3e679355e44d8035
Suy65 <115409094+Suy65@users.noreply.github.com>
2023-06-16 16:03:36 +0530

    Time: 527 ms (31.95%), Space: 178.6 MB (16.18%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -4,57 +4,57 @@ public:
         public:
         int key;
         int value;
-        node *next;
-        node *prev;
-        node(int _key ,int _val){
-            key=_key;
-            value=_val;
+        node*prev;
+        node*next;
+        node(int _key ,int _value){
+            key =_key;
+            value =_value;
         }
     };
-    node *head=new node(-1,-1);
-    node *tail=new node(-1,-1);
-    int cap;
+    node *head = new node(-1,-1);
+    node *tail = new node(-1,-1);
+    int cap  ;
     unordered_map<int,node*>m;
-    
     LRUCache(int capacity) {
-        cap=capacity;
-        head->next=tail;
-        tail->prev=head;
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;
     }
-    void addNode(node *newnode){
-        node *temp=head->next;
-        newnode->next=temp;
-        newnode->prev=head;
-        head->next=newnode;
-        temp->prev=newnode;
-    } 
-    void deleteNode(node* delnode){
-        node*delprev=delnode->prev;
-        node*delnext=delnode->next;
-        delprev->next=delnext;
-        delnext->prev=delprev;
+    void addNode(node *newNode){
+        node *temp =head->next;
+        newNode->next =temp ;
+        newNode->prev =head;
+        head->next = newNode;
+        temp->prev =newNode;
     }
-    
+    void deleteNode(node *delNode){
+        node *delprev = delNode ->prev;
+        node *delnext = delNode->next;
+        delprev->next = delnext;
+        delnext->prev = delprev;
+    }
     int get(int key) {
-        if(m.find(key)!=m.end()){
-            node *resnode= m[key];
-            int res=resnode->value;
+        if(m.find(key) != m.end()){
+            node *resnode = m[key];
+            int res =resnode->value;
             m.erase(key);
             deleteNode(resnode);
             addNode(resnode);
-            m[key]=head->next;
+            m[key]= head->next;
             return res;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        if(m.find(key)!=m.end()){
-            node*existingnode=m[key];
-            m.erase(key);
-            deleteNode(existingnode);
-        }
-        if(m.size()==cap){
+        
+    if(m.find(key) != m.end()){
+        node *existingnode =m[key];
+        deleteNode(existingnode);
+        m.erase(key);
+        
+      }
+        if(m.size() == cap){
             m.erase(tail->prev->key);
             deleteNode(tail->prev);
         }
