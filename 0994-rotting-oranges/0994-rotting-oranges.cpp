c6b0f3a98048e736c529818e4b10af2a16950f88
Suy65 <115409094+Suy65@users.noreply.github.com>
2023-06-16 22:48:37 +0530

    Time: 7 ms (73.61%), Space: 13.1 MB (73.72%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,6 +5,7 @@ public:
         int m = grid[0].size();
         queue<pair<pair<int,int>,int>>q;
         int vis[n][m];
+        int cntfresh =0;
         for( int i=0 ;i< n;i++){
             for( int j=0 ;j<m;j++){
                 if(grid[i][j] == 2){
@@ -14,11 +15,13 @@ public:
                 }else{
                     vis[i][j]=0;
                 }
+                if(grid[i][j]==1)cntfresh++;
             }
         }
         int tm =0;
         int drow[]={-1,0,1,0};
         int dcol[]={0,1,0,-1};
+        int cnt =0;
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
@@ -31,14 +34,18 @@ public:
                 if(nrow >=0 && nrow<n && ncol >=0 && ncol< m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2){
                     q.push({{nrow,ncol},t+1});
                     vis[nrow][ncol]=2;
+                    cnt++;
                 }
             }
         }
-        for( int i=0 ; i<n;i++){
-          for( int j=0 ; j<m;j++){
-              if( grid[i][j] == 1 && vis[i][j]!=2)
-                  return -1;
-           }   
+ //       for( int i=0 ; i<n;i++){
+ //         for( int j=0 ; j<m;j++){
+ //             if( grid[i][j] == 1 && vis[i][j]!=2)
+ //                 return -1;
+ //          }   
+ //       }
+        if(cntfresh != cnt){
+            return -1;
         }
         return tm;
     }
