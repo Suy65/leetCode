626a12ed16c90d568cb98f762a74eb8ced5ecbbe
Suy65 <115409094+Suy65@users.noreply.github.com>
2023-06-18 23:12:00 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (54.28%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         int m =matrix[0].size();
         // TAKE TRANSPOSE OF MATRIX
         for( int i =0;i<n;i++){
-            for( int j =0;j<i;j++){
+            for( int j =0;j<=i;j++){
                 swap(matrix[i][j] , matrix[j][i]);
             }
         }
